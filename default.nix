b45811a8de2000dc5e04e1ca40e142596b652c09
axel paulander <axel.paulander@gmail.com>
2023-09-08 03:33:21 +0200

    [latex] Run watcher in tmp

--- default.nix
+++ default.nix
@@ -1,23 +1,14 @@
 { pkgs }:
 rec {
 
-  #FIXME works for ./. ? 
-  pathToStr = with builtins; nsp:
-    let m = match "/nix/store/[^-]+-(.*)" "${nsp}"; in
-    if m == null then nsp else (head m);
-
-  buildLatex = { index, fonts, flags ? "" }: ''
-    mkdir -p .cache/texmf-var
-    env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
+  buildLatex = { src ? ".", index, cache ? ".cache", fonts, flags ? "" }: ''
+    env TEXMFHOME="${cache}" TEXMFVAR="${cache}/texmf-var" \
       ${if fonts != null then "OSFONTDIR=${fonts}/share/fonts" else ""} \
-      latexmk -interaction=nonstopmode -pdf -lualatex -file-line-error ${flags} \
-      ${index}.tex \
+      latexmk -interaction=nonstopmode -pdf -lualatex -cd -file-line-error ${flags} \
+      ${src}/${index}.tex \
   '';
 
   mkLatexProject = { name, src ? ./src, latexInputs, index ? "index", fonts ? null }:
-    let
-      raw_src = pathToStr src;
-    in
     rec
     {
       build =
@@ -26,6 +17,7 @@ rec {
           buildInputs = [ pkgs.coreutils latexInputs ];
           phases = [ "unpackPhase" "buildPhase" "installPhase" ];
           buildPhase = ''
+            mkdir -p .cache/texmf-var
             export SOURCE_DATE_EPOCH=$(date +%s);
             export PATH="${pkgs.lib.makeBinPath buildInputs}";
             ${buildLatex {inherit index fonts;}}
@@ -40,12 +32,15 @@ rec {
         name = "Watch-${name}";
         runtimeInputs = [ pkgs.coreutils latexInputs pkgs.zathura ];
         text = ''
-          cd ${raw_src}
+          echo "Making auxdir..."
+          auxdir=$(mktemp -d -t XXX-${name})
+          mkdir -p "$auxdir/texmf-var"
+          echo "Made auxdir '$auxdir'"
+
           # Watch
-          ${buildLatex {inherit index fonts; flags='' -pvc -e "\$pdf_previewer='zathura'" '';}}
+          ${buildLatex {inherit src index fonts; cache="$auxdir"; flags='' -pvc -e "\$pdf_previewer='zathura'" -outdir="$auxdir"'';}}
           # Cleanup aux and similar since apps run locally and not in nix-store
-          latexmk -C ${index}.tex
-          rm -r .cache
+          rm -r "$auxdir"
         '';
       };
 
