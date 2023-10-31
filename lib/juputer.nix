1dabd8b92c9591350dc82875b6f69c6f6db32d09
axel paulander <axel.paulander@gmail.com>
2023-10-31 19:53:59 +0100

    [jupyter] add jupyter helper to lib

---
+++ lib/juputer.nix
@@ -0,0 +1,21 @@
+{ pkgs }: {
+  mkJupyter = { name ? "jupyter", python ? "python310", withPackages ? ps: [ ], lab ? false }:
+    let
+      pythonPackages = python + "Packages";
+    in
+    pkgs.mkShell
+      {
+        inherit name;
+        buildInputs = [
+          (if lab then
+            pkgs.${pythonPackages}.jupyterlab
+          else
+            pkgs.${pythonPackages}.jupyter)
+          pkgs.${pythonPackages}.pip
+          (pkgs.${python}.withPackages withPackages)
+        ];
+        shellHook = ''
+          jupyter ${if lab then "lab" else "notebook"} && exit # exit nix-shell when closing jupyter
+        '';
+      };
+}
