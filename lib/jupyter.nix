0e322f7c1c146e76d172a9becfce26d3803f8ad4
axel paulander <axel.paulander@gmail.com>
2024-06-15 15:44:00 +0200

    Attempt at allowing ovverides in python

--- lib/jupyter.nix
+++ lib/jupyter.nix
@@ -1,40 +1,53 @@
-{ pkgs }: {
-  mkJupyter = { name ? "jupyter", python ? "python310", withPackages ? ps: [ ], lab ? false }:
-    let
-      pythonPackages = python + "Packages";
-    in
+{pkgs}: {
+  mkJupyter = {
+    name ? "jupyter",
+    python,
+    withPackages ? ps: [],
+    lab ? false,
+  }:
     pkgs.mkShell
-      {
-        inherit name;
-        buildInputs = [
-          (if lab then
-            pkgs.${pythonPackages}.jupyterlab
-          else
-            pkgs.${pythonPackages}.jupyter)
-          pkgs.${pythonPackages}.pip
-          (pkgs.${python}.withPackages withPackages)
-        ];
-        shellHook = ''
-          jupyter ${if lab then "lab --LabApp.extension_manager=readonly" else "notebook"} && exit # exit nix-shell when closing jupyter
-        '';
-      };
-
-  labApp = { name ? "jupyter-lab", python ? "python310", withPackages ? ps: [ ], extraArgs ? "" }:
-    let
-      pythonPackages = python + "Packages";
-      lab = pkgs.writeShellApplication {
-        inherit name;
-        runtimeInputs = [
-          pkgs.${pythonPackages}.jupyterlab
-          (pkgs.${python}.withPackages withPackages)
-        ];
-        text = '' 
-              jupyter-lab --LabApp.extension_manager=readonly ${extraArgs} .
-          '';
-      };
-    in
     {
-      type = "app";
-      program = "${lab}/bin/${lab.name}";
+      inherit name;
+      buildInputs = [
+        (python.withPackages (ps:
+          with ps; [
+            pip
+            (
+              if lab
+              then jupyterlab
+              else jupyter
+            )
+          ]))
+
+        (python.withPackages withPackages)
+      ];
+      shellHook = ''
+        jupyter ${
+          if lab
+          then "lab --LabApp.extension_manager=readonly"
+          else "notebook"
+        } && exit # exit nix-shell when closing jupyter
+      '';
     };
+
+  labApp = {
+    name ? "jupyter-lab",
+    python,
+    withPackages ? ps: [],
+    extraArgs ? "",
+  }: let
+    lab = pkgs.writeShellApplication {
+      inherit name;
+      runtimeInputs = [
+        (pkgs.${python}.withPackages (ps: [ps.jupyterlab]))
+        (pkgs.${python}.withPackages withPackages)
+      ];
+      text = ''
+        jupyter-lab --LabApp.extension_manager=readonly ${extraArgs} .
+      '';
+    };
+  in {
+    type = "app";
+    program = "${lab}/bin/${lab.name}";
+  };
 }
